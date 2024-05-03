2c78a01bfb84947bd4a59fe1167ca226345aac1a
dan9229 <166366722+dan9229@users.noreply.github.com>
2024-05-03 13:44:51 +0800

    update

--- header.php
+++ header.php
@@ -1,35 +1,35 @@
-<a href="index.html"><img src="images/logo.jpg" id="logo" alt="logo"></img></a>
+<a href="index.php"><img src="images/logo.jpg" id="logo" alt="logo"></img></a>
   <input class="side-menu" type="checkbox" id="side-menu">
   <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
   <nav>
-      <a href="index.html"><img src="images/logo.jpg" alt="logo" id="navbar_logo"></a>
+      <a href="index.php"><img src="images/logo.jpg" alt="logo" id="navbar_logo"></a>
       <ul class="navbar">
-          <li><a href="index.html">Home</a></li>
+          <li><a href="index.php">Home</a></li>
           <li class="subnav">
           <a href="#">Services <i class="fa fa-caret-down"></i></a>
           <ul class="submenu">
-              <li><a href="service1.html">BIM Class</a></li>
-              <li><a href="service2.html">Car Wash</a></li>
-              <li><a href="service3.html">Haircut & Trimming</a></li>
-              <li><a href="service4.html">Sewing & Alteration</a></li>
+              <li><a href="service1.php">BIM Class</a></li>
+              <li><a href="service2.php">Car Wash</a></li>
+              <li><a href="service3.php">Haircut & Trimming</a></li>
+              <li><a href="service4.php">Sewing & Alteration</a></li>
           </ul>
           </li>
-          <li><a href="activities.html">Activities</a></li>
+          <li><a href="activities.php">Activities</a></li>
           <li class="subnav">
           <a href="#">Forms <i class="fa fa-caret-down"></i></a>
           <ul class="submenu">
-              <li><a href="enquiry-service">Enquiry</a></li>
-              <li><a href="join-volunteer.html">Volunteer</a></li>
+              <li><a href="enquiry-service.php">Enquiry</a></li>
+              <li><a href="join-volunteer.php">Volunteer</a></li>
           </ul>
           </li>
           <li class="subnav">
           <a href="#">About Us <i class="fa fa-caret-down"></i></a>
           <ul class="submenu">
-              <li><a href="daniel.html">&#128100; Daniel Sie</a></li>
-              <li><a href="michael.html">&#128100; Michael Wong</a></li>
-              <li><a href="paing.html">&#128100; Paing Chan</a></li>
-              <li><a href="sherlyn.html">&#128100; Sherlyn Kok</a></li>
-              <li><a href="zwe.html">&#128100; Zwe Htet Zaw</a></li>
+              <li><a href="daniel.php">&#128100; Daniel Sie</a></li>
+              <li><a href="michael.php">&#128100; Michael Wong</a></li>
+              <li><a href="paing.php">&#128100; Paing Chan</a></li>
+              <li><a href="sherlyn.php">&#128100; Sherlyn Kok</a></li>
+              <li><a href="zwe.php">&#128100; Zwe Htet Zaw</a></li>
           </ul>
           </li>
       </ul>
