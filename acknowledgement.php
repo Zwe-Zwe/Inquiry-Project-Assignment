88db3e4c4d07c6fcb750b95d4fa4d9241aa39239
dan9229 <166366722+dan9229@users.noreply.github.com>
2024-05-03 13:27:15 +0800

    changing header into php

---
+++ acknowledgement.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Malaysian Sign Language</title>
+    <meta charset="utf-8">
+    <meta name="description" content="acknowledgement">
+    <meta name="keywords" content="acknowledgement">
+    <meta name="author" content="Daniel Sie, Zwe Htet Zaw, Paing Chan, Sherlyn Kok, Michael Wong">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+      rel="icon"
+      href="images/love-you-gesture-svgrepo-com.svg"
+      type="images/svg"
+    >
+    <link rel="stylesheet" href="styles/style.css">
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    >
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    >
+  </head>
+
+  <body>
+    <header>
+      <?php
+        include "header.php"
+      ?>
+    </header>
+
+    <section class="acknowledgement-section">
+      <div class="acknowledgement-container">
+        <h1>Acknowledgements</h1>
+        <div class="acknowledgements">
+          <h2>MALAYSIAN SIGN LANGUAGE MEDICAL DATASET</h2>
+          <h3>Special Thanks:</h3>
+          <ul>
+            <li>Ts. Dr. Sue Han Lee</li>
+            <li>Alec Vince</li>
+            <li>Masrur Rahman</li>
+            <li>Alison Dwyne</li>
+          </ul>
+          <h3>Source</h3>
+          <ul>
+            <li>
+              <a href="https://arekku21.github.io/MSL-Medical/#">Click Here</a>
+              for more info
+            </li>
+          </ul>
+        </div>
+
+        <hr />
+
+        <div class="acknowledgements">
+          <h2>Sarawak Society for the Deaf (Facebook)</h2>
+          <h3>Source</h3>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/sarawaksocietyforthedeaf"
+                >Click Here</a
+              >
+              for more info
+            </li>
+          </ul>
+        </div>
+
+        <hr />
+
+        <div class="acknowledgements">
+          <h2>Sarawak Society for the Deaf (Website)</h2>
+          <h3>Source</h3>
+          <ul>
+            <li>
+              <a href="https://www.sarawaksocietyforthedeaf.org/">Click Here</a>
+              for more info
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <a href="#" class="top">Back To Top</a>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4 class="foot-h4-oh">operating hours</h4>
+            <ul>
+              <li>Monday to Friday 8am- 5pm</li>
+              <li>Saturday 9am- 5pm</li>
+              <li>Sunday Closed</li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>organization</h4>
+            <ul>
+              <li><a href="activities.html">activities</a></li>
+              <li><a href="enquiry-service.html">enquiry</a></li>
+              <li><a href="join-volunteer.html">join us</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>profiles</h4>
+            <ul>
+              <li><a href="daniel.html">Daniel Sie</a></li>
+              <li><a href="michael.html">Michael Wong</a></li>
+              <li><a href="paing.html">Paing Chan</a></li>
+              <li><a href="sherlyn.html">Sherlyn Kok</a></li>
+              <li><a href="zwe.html">Zwe Htet Zaw</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>contact us</h4>
+            <div class="social-links">
+              <a href="https://www.facebook.com/sarawaksocietyforthedeaf"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+              <a href="https://www.sarawaksocietyforthedeaf.org/"
+                ><i class="fa-solid fa-globe"></i
+              ></a>
+              <a href="https://wa.me/+6012-8118260"
+                ><i class="fa-brands fa-whatsapp"></i
+              ></a>
+              <a href="mailto:102783895@students.swinburne.edu.my"
+                ><i class="fa-solid fa-envelope"></i
+              ></a>
+              <br />
+            </div>
+            <a class="footer-essentials" href="acknowledgement.html"
+              >Acknowledgements</a
+            >
+            <a class="footer-essentials" href="enhancement1.html"
+              >Enhancements</a
+            >
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<a href="index.html"><img src="images/logo.jpg" id="logo"></img></a>
+  <input class="side-menu" type="checkbox" id="side-menu">
+  <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+  <nav>
+      <a href="index.html"><img src="images/logo.jpg" alt="logo" id="navbar_logo"></a>
+      <ul class="navbar">
+          <li><a href="index.html">Home</a></li>
+          <li class="subnav">
+          <a href="#">Services <i class="fa fa-caret-down"></i></a>
+          <ul class="submenu">
+              <li><a href="service1.html">BIM Class</a></li>
+              <li><a href="service2.html">Car Wash</a></li>
+              <li><a href="service3.html">Haircut & Trimming</a></li>
+              <li><a href="service4.html">Sewing & Alteration</a></li>
+          </ul>
+          </li>
+          <li><a href="activities.html">Activities</a></li>
+          <li class="subnav">
+          <a href="#">Forms <i class="fa fa-caret-down"></i></a>
+          <ul class="submenu">
+              <li><a href="enquiry-service">Enquiry</a></li>
+              <li><a href="join-volunteer.html">Volunteer</a></li>
+          </ul>
+          </li>
+          <li class="subnav">
+          <a href="#">About Us <i class="fa fa-caret-down"></i></a>
+          <ul class="submenu">
+              <li><a href="daniel.html">&#128100; Daniel Sie</a></li>
+              <li><a href="michael.html">&#128100; Michael Wong</a></li>
+              <li><a href="paing.html">&#128100; Paing Chan</a></li>
+              <li><a href="sherlyn.html">&#128100; Sherlyn Kok</a></li>
+              <li><a href="zwe.html">&#128100; Zwe Htet Zaw</a></li>
+          </ul>
+          </li>
+      </ul>
+    </nav>
